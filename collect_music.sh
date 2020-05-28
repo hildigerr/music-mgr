b685e32775529c5ca8f4335098c52d1aa9c9d6e0
Hildigerr Vergaray <Hildigerr.Vergaray@YmirSystems.com>
2020-05-28 16:22:50 -0700

    get file data then verify in phases

--- collect_music.sh
+++ collect_music.sh
@@ -9,11 +9,11 @@
 
 ### TODO:
 # Write/update tags including arbitrary tag fields.
+# Add auto yes iscorrect option.
 # Add option to convert without asking.
 # Convert to ogg instead or also or as requested or whatever.
 # Move converted wav files to workdir/old/wav or something like that.
 # Make an option which will just clean up workdir.
-# See other comments for notes during work in progress.
 
 usage() { echo "Usage: $0 [-v] [-s <source directory>] [-t <target directory>]" 1>&2; exit 1; }
 nodir() { echo "ERROR: $1 does not exist or is not a directory." 1>&2; exit 2; }
@@ -59,6 +59,7 @@ while read -r line || [[ -n ${line} ]]; do
     echo -e "\nFile: ${line}"
 
     ### Get Basic File Data ###
+    filename=`basename "${line}"`
     data=`mediainfo "${line}"`
     format=`grep -m 1 "Format" <<<"${data}" | cut -d: -f 2- | sed 's/^ *//g'`
 
@@ -68,27 +69,51 @@ while read -r line || [[ -n ${line} ]]; do
         read -s -n 1 -p "Do you want to convert \"${line}\" to MP3 (y/n)? " doconvert < /dev/tty && echo #skip it if it has already been done
         if [ "${doconvert}" = 'y' ] || [ "${doconvert}" = 'Y' ] ; then
             lame -h -b128 "${line}" "${title}.mp3" && line="${title}.mp3"
-            while true ; do
-                read -s -n 1 -p "Is \"${title}\" the correct song title (y/n)? " iscorrect < /dev/tty && echo #add yes as default
-                if [ "${iscorrect}" = 'y' ] || [ "${iscorrect}" = 'Y' ] ; then
-                    break
-                else
-                    read -p "Enter the song title: " title < /dev/tty
-                fi
-            done
         fi
-        read -p "Enter the album artist: " artist < /dev/tty
-        #if empty make "Various Artists"?
-        read -p "Enter the song album: " album < /dev/tty
-        #if empty make "Unknown"
     else
         title=`grep -m 1 "Track name " <<<"${data}" | cut -d: -f 2- | sed 's/^ *//g'`
-        #if empty make basename ; now verify
         artist=`grep -m 1 "Performer" <<<"${data}" | cut -d: -f 2- | sed 's/^ *//g'`
-        #if empty make remainder or head of filename ; now verify
         album=`grep -m 1 "Album" <<<"${data}" | cut -d: -f 2- | sed 's/^ *//g'`
-        #if empty make "Unknown" ; now verify
     fi
+
+    ### Verify Title ###
+    if  [ -z "${title}" ] ; then
+        title=${filename}
+    fi
+    while true ; do
+        read -s -n 1 -p "Is \"${title}\" the correct song title (y/n)? " iscorrect < /dev/tty && echo
+        if [ "${iscorrect}" = 'y' ] || [ "${iscorrect}" = 'Y' ] ; then
+            break
+        else
+            read -p "Enter the song title: " title < /dev/tty
+        fi
+    done
+
+    ### Verify Artist ###
+    if [ -z "${artist}" ] ; then
+        artist="Various Artists"
+    fi
+    while true ; do
+        read -s -n 1 -p "Is \"${artist}\" the correct album artist (y/n)? " iscorrect < /dev/tty && echo
+        if [ "${iscorrect}" = 'y' ] || [ "${iscorrect}" = 'Y' ] ; then
+            break
+        else
+            read -p "Enter the album artist: " artist < /dev/tty
+        fi
+    done
+
+    ### Verify Album ###
+    if [ -z "${album}" ] ; then
+        album="Unknown Album"
+    fi
+    while true ; do
+        read -s -n 1 -p "Is \"${album}\" the correct album name (y/n)? " iscorrect < /dev/tty && echo
+        if [ "${iscorrect}" = 'y' ] || [ "${iscorrect}" = 'Y' ] ; then
+            break
+        else
+            read -p "Enter the album name: " album < /dev/tty
+        fi
+    done
 
     if ${verbose} ; then
         echo
