65b62387d4f6ff4948e1a81d5c47ae9cf8c893ca
Hildigerr Vergaray <Hildigerr.Vergaray@YmirSystems.com>
2026-03-17 13:48:34 -0700

    trap SIGUSR1 for pause/resume

--- play_music.sh
+++ play_music.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]; then $0 "$HOME/Music/"; fi
+toggle_pause() {
+  if [ -n "${ppid}" ] && ps -p "${ppid}" >/dev/null; then
+    if ps -o state= -p "${ppid}" | grep -q "T"; then
+      kill -CONT "${ppid}"
+    else
+      kill -STOP "${ppid}"
+    fi
+  fi
+}
+
+trap toggle_pause SIGUSR1
+
+if [ $# -eq 0 ]; then exec "$0" "$HOME/Music/"; fi
 
 for each in "$@"; do
+  unset ppid
   if [ -d "${each}" ] ; then
     echo "Generating Random Playlist: \"${each}\""
     find "${each}" -type f -print0 | sort -Rz | xargs -0 $0
@@ -17,14 +30,23 @@ for each in "$@"; do
         chafa "${each}" 2>/dev/null ;; #XXX
       audio/midi)
         echo "\n${each}\n"
-        fluidsynth -i "${each}" 2>/dev/null ;;
+        fluidsynth -i "${each}" 2>/dev/null &
+        ppid=$!
+        while kill -0 "${ppid}" 2>/dev/null; do
+          wait "${ppid}" 2>/dev/null
+        done
+      ;;
       audio/flac) ;&
       audio/mpeg) ;&
       audio/ogg) ;&
       audio/x-aiff) ;&
       audio/x-m4a) ;&
       audio/x-wav)
-        play "${each}"
+        play "${each}" &
+        ppid=$!
+        while kill -0 "${ppid}" 2>/dev/null; do
+          wait "${ppid}" 2>/dev/null
+        done
       ;;
       *) echo "Unhandled Filetype: \"${each}\"" ;;
     esac
